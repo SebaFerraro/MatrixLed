892f76ebb77d656d2920fff48a429226c9327e83
sferraro <sferraro@sfnet>
2017-11-15 04:16:44 +0000

    Con Lectura de Temperatura

--- build/main/SensTemp.d
+++ build/main/SensTemp.d
@@ -1,13 +1,18 @@
-MatrixLed.o: /home/sferraro/esp32/MatrixLed/main/./MatrixLed.cpp \
+SensTemp.o: /home/sferraro/esp32/MatrixLed/main/./SensTemp.cpp \
+ /home/sferraro/esp32/esp-idf/components/driver/include/driver/i2c.h \
+ /home/sferraro/esp32/esp-idf/components/esp32/include/esp_types.h \
+ /home/sferraro/esp32/esp-idf/components/newlib/include/sys/cdefs.h \
+ /home/sferraro/esp32/esp-idf/components/newlib/include/machine/_default_types.h \
+ /home/sferraro/esp32/esp-idf/components/newlib/include/sys/features.h \
+ /home/sferraro/esp32/esp-idf/components/newlib/include/stdint.h \
+ /home/sferraro/esp32/esp-idf/components/newlib/include/sys/_intsup.h \
+ /home/sferraro/esp32/esp-idf/components/esp32/include/esp_err.h \
  /home/sferraro/esp32/esp-idf/components/newlib/include/stdio.h \
  /home/sferraro/esp32/esp-idf/components/newlib/include/_ansi.h \
  /home/sferraro/esp32/esp-idf/components/newlib/include/newlib.h \
  /home/sferraro/esp32/esp-idf/components/newlib/include/sys/config.h \
  /home/sferraro/esp32/esp-idf/components/newlib/include/machine/ieeefp.h \
- /home/sferraro/esp32/esp-idf/components/newlib/include/sys/features.h \
  /home/sferraro/esp32/esp-idf/components/esp32/include/xtensa/config/core-isa.h \
- /home/sferraro/esp32/esp-idf/components/newlib/include/sys/cdefs.h \
- /home/sferraro/esp32/esp-idf/components/newlib/include/machine/_default_types.h \
  /home/sferraro/esp32/esp-idf/components/newlib/include/sys/reent.h \
  /home/sferraro/esp32/esp-idf/components/newlib/include/_ansi.h \
  /home/sferraro/esp32/esp-idf/components/newlib/include/sys/_types.h \
@@ -16,11 +21,15 @@ MatrixLed.o: /home/sferraro/esp32/MatrixLed/main/./MatrixLed.cpp \
  /home/sferraro/esp32/esp-idf/components/newlib/include/sys/types.h \
  /home/sferraro/esp32/esp-idf/components/newlib/include/machine/types.h \
  /home/sferraro/esp32/esp-idf/components/newlib/include/sys/stdio.h \
- /home/sferraro/esp32/esp-idf/components/freertos/include/freertos/FreeRTOS.h \
- /home/sferraro/esp32/esp-idf/components/newlib/include/stdint.h \
- /home/sferraro/esp32/esp-idf/components/newlib/include/sys/_intsup.h \
- /home/sferraro/esp32/esp-idf/components/freertos/include/freertos/FreeRTOSConfig.h \
+ /home/sferraro/esp32/esp-idf/components/newlib/platform_include/assert.h \
  /home/sferraro/esp32/MatrixLed/build/include/sdkconfig.h \
+ /home/sferraro/esp32/esp-idf/components/newlib/include/stdlib.h \
+ /home/sferraro/esp32/esp-idf/components/newlib/include/machine/stdlib.h \
+ /home/sferraro/esp32/esp-idf/components/newlib/include/alloca.h \
+ /home/sferraro/esp32/esp-idf/components/newlib/include/assert.h \
+ /home/sferraro/esp32/esp-idf/components/esp32/include/esp_intr_alloc.h \
+ /home/sferraro/esp32/esp-idf/components/freertos/include/freertos/FreeRTOS.h \
+ /home/sferraro/esp32/esp-idf/components/freertos/include/freertos/FreeRTOSConfig.h \
  /home/sferraro/esp32/esp-idf/components/freertos/include/freertos/xtensa_config.h \
  /home/sferraro/esp32/esp-idf/components/esp32/include/xtensa/hal.h \
  /home/sferraro/esp32/esp-idf/components/esp32/include/xtensa/config/core.h \
@@ -31,14 +40,9 @@ MatrixLed.o: /home/sferraro/esp32/MatrixLed/main/./MatrixLed.cpp \
  /home/sferraro/esp32/esp-idf/components/freertos/include/freertos/xtensa_context.h \
  /home/sferraro/esp32/esp-idf/components/esp32/include/xtensa/corebits.h \
  /home/sferraro/esp32/esp-idf/components/esp32/include/xtensa/xtruntime-frames.h \
- /home/sferraro/esp32/esp-idf/components/newlib/include/stdlib.h \
- /home/sferraro/esp32/esp-idf/components/newlib/include/machine/stdlib.h \
- /home/sferraro/esp32/esp-idf/components/newlib/include/alloca.h \
  /home/sferraro/esp32/esp-idf/components/esp32/include/rom/ets_sys.h \
  /home/sferraro/esp32/esp-idf/components/soc/esp32/include/soc/soc.h \
  /home/sferraro/esp32/esp-idf/components/esp32/include/esp_assert.h \
- /home/sferraro/esp32/esp-idf/components/newlib/platform_include/assert.h \
- /home/sferraro/esp32/esp-idf/components/newlib/include/assert.h \
  /home/sferraro/esp32/esp-idf/components/freertos/include/freertos/projdefs.h \
  /home/sferraro/esp32/esp-idf/components/freertos/include/freertos/portable.h \
  /home/sferraro/esp32/esp-idf/components/freertos/include/freertos/deprecated_definitions.h \
@@ -50,16 +54,12 @@ MatrixLed.o: /home/sferraro/esp32/MatrixLed/main/./MatrixLed.cpp \
  /home/sferraro/esp32/esp-idf/components/esp32/include/esp_crosscore_int.h \
  /home/sferraro/esp32/esp-idf/components/freertos/include/freertos/portbenchmark.h \
  /home/sferraro/esp32/esp-idf/components/freertos/include/freertos/mpu_wrappers.h \
- /home/sferraro/esp32/esp-idf/components/freertos/include/freertos/task.h \
- /home/sferraro/esp32/esp-idf/components/newlib/include/limits.h \
- /home/sferraro/esp32/esp-idf/components/freertos/include/freertos/list.h \
- /home/sferraro/esp32/esp-idf/components/driver/include/driver/i2c.h \
- /home/sferraro/esp32/esp-idf/components/esp32/include/esp_types.h \
- /home/sferraro/esp32/esp-idf/components/esp32/include/esp_err.h \
- /home/sferraro/esp32/esp-idf/components/esp32/include/esp_intr_alloc.h \
  /home/sferraro/esp32/esp-idf/components/freertos/include/freertos/semphr.h \
  /home/sferraro/esp32/esp-idf/components/freertos/include/freertos/queue.h \
  /home/sferraro/esp32/esp-idf/components/freertos/include/freertos/xtensa_api.h \
+ /home/sferraro/esp32/esp-idf/components/freertos/include/freertos/task.h \
+ /home/sferraro/esp32/esp-idf/components/newlib/include/limits.h \
+ /home/sferraro/esp32/esp-idf/components/freertos/include/freertos/list.h \
  /home/sferraro/esp32/esp-idf/components/freertos/include/freertos/queue.h \
  /home/sferraro/esp32/esp-idf/components/freertos/include/freertos/ringbuf.h \
  /home/sferraro/esp32/esp-idf/components/driver/include/driver/gpio.h \
@@ -71,44 +71,23 @@ MatrixLed.o: /home/sferraro/esp32/MatrixLed/main/./MatrixLed.cpp \
  /home/sferraro/esp32/esp-idf/components/soc/esp32/include/soc/gpio_sig_map.h \
  /home/sferraro/esp32/esp-idf/components/esp32/include/rom/gpio.h \
  /home/sferraro/esp32/esp-idf/components/esp32/include/esp_attr.h \
- /home/sferraro/esp32/esp-idf/components/esp32/include/esp_wifi.h \
- /home/sferraro/esp32/esp-idf/components/esp32/include/rom/queue.h \
- /home/sferraro/esp32/esp-idf/components/esp32/include/esp_wifi_types.h \
- /home/sferraro/esp32/esp-idf/components/esp32/include/esp_interface.h \
- /home/sferraro/esp32/esp-idf/components/esp32/include/esp_wifi_crypto_types.h \
- /home/sferraro/esp32/esp-idf/components/esp32/include/esp_event.h \
- /home/sferraro/esp32/esp-idf/components/tcpip_adapter/include/tcpip_adapter.h \
- /home/sferraro/esp32/esp-idf/components/esp32/include/rom/queue.h \
- /home/sferraro/esp32/esp-idf/components/esp32/include/esp_wifi_types.h \
- /home/sferraro/esp32/esp-idf/components/lwip/include/lwip/lwip/ip_addr.h \
- /home/sferraro/esp32/esp-idf/components/lwip/include/lwip/lwip/opt.h \
- /home/sferraro/esp32/esp-idf/components/lwip/include/lwip/port/lwipopts.h \
- /home/sferraro/esp32/esp-idf/components/newlib/include/time.h \
- /home/sferraro/esp32/esp-idf/components/newlib/include/machine/time.h \
- /home/sferraro/esp32/esp-idf/components/newlib/include/sys/time.h \
- /home/sferraro/esp32/esp-idf/components/newlib/include/sys/fcntl.h \
- /home/sferraro/esp32/esp-idf/components/newlib/include/sys/_default_fcntl.h \
- /home/sferraro/esp32/esp-idf/components/newlib/include/sys/stat.h \
- /home/sferraro/esp32/esp-idf/components/esp32/include/esp_task.h \
- /home/sferraro/esp32/esp-idf/components/lwip/include/lwip/lwip/debug.h \
- /home/sferraro/esp32/esp-idf/components/lwip/include/lwip/lwip/arch.h \
- /home/sferraro/esp32/esp-idf/components/lwip/include/lwip/port/arch/cc.h \
- /home/sferraro/esp32/esp-idf/components/newlib/include/errno.h \
- /home/sferraro/esp32/esp-idf/components/newlib/include/sys/errno.h \
- /home/sferraro/esp32/esp-idf/components/lwip/include/lwip/port/arch/sys_arch.h \
- /home/sferraro/esp32/esp-idf/components/lwip/include/lwip/lwip/def.h \
- /home/sferraro/esp32/esp-idf/components/lwip/include/lwip/lwip/ip4_addr.h \
- /home/sferraro/esp32/esp-idf/components/lwip/include/lwip/lwip/ip6_addr.h \
- /home/sferraro/esp32/esp-idf/components/lwip/include/lwip/apps/dhcpserver.h \
- /home/sferraro/esp32/MatrixLed/main/./caracteres.h \
- /home/sferraro/esp32/esp-idf/components/esp32/include/esp_system.h \
- /home/sferraro/esp32/esp-idf/components/esp32/include/esp_deep_sleep.h \
- /home/sferraro/esp32/esp-idf/components/driver/include/driver/touch_pad.h \
- /home/sferraro/esp32/esp-idf/components/esp32/include/esp_intr.h \
- /home/sferraro/esp32/esp-idf/components/esp32/include/rom/ets_sys.h \
- /home/sferraro/esp32/esp-idf/components/nvs_flash/include/nvs_flash.h \
- /home/sferraro/esp32/esp-idf/components/nvs_flash/include/nvs.h \
  /home/sferraro/esp32/esp-idf/components/log/include/esp_log.h
+
+/home/sferraro/esp32/esp-idf/components/driver/include/driver/i2c.h:
+
+/home/sferraro/esp32/esp-idf/components/esp32/include/esp_types.h:
+
+/home/sferraro/esp32/esp-idf/components/newlib/include/sys/cdefs.h:
+
+/home/sferraro/esp32/esp-idf/components/newlib/include/machine/_default_types.h:
+
+/home/sferraro/esp32/esp-idf/components/newlib/include/sys/features.h:
+
+/home/sferraro/esp32/esp-idf/components/newlib/include/stdint.h:
+
+/home/sferraro/esp32/esp-idf/components/newlib/include/sys/_intsup.h:
+
+/home/sferraro/esp32/esp-idf/components/esp32/include/esp_err.h:
 
 /home/sferraro/esp32/esp-idf/components/newlib/include/stdio.h:
 
@@ -120,13 +99,7 @@ MatrixLed.o: /home/sferraro/esp32/MatrixLed/main/./MatrixLed.cpp \
 
 /home/sferraro/esp32/esp-idf/components/newlib/include/machine/ieeefp.h:
 
-/home/sferraro/esp32/esp-idf/components/newlib/include/sys/features.h:
-
 /home/sferraro/esp32/esp-idf/components/esp32/include/xtensa/config/core-isa.h:
-
-/home/sferraro/esp32/esp-idf/components/newlib/include/sys/cdefs.h:
-
-/home/sferraro/esp32/esp-idf/components/newlib/include/machine/_default_types.h:
 
 /home/sferraro/esp32/esp-idf/components/newlib/include/sys/reent.h:
 
@@ -144,15 +117,23 @@ MatrixLed.o: /home/sferraro/esp32/MatrixLed/main/./MatrixLed.cpp \
 
 /home/sferraro/esp32/esp-idf/components/newlib/include/sys/stdio.h:
 
-/home/sferraro/esp32/esp-idf/components/freertos/include/freertos/FreeRTOS.h:
-
-/home/sferraro/esp32/esp-idf/components/newlib/include/stdint.h:
-
-/home/sferraro/esp32/esp-idf/components/newlib/include/sys/_intsup.h:
-
-/home/sferraro/esp32/esp-idf/components/freertos/include/freertos/FreeRTOSConfig.h:
+/home/sferraro/esp32/esp-idf/components/newlib/platform_include/assert.h:
 
 /home/sferraro/esp32/MatrixLed/build/include/sdkconfig.h:
+
+/home/sferraro/esp32/esp-idf/components/newlib/include/stdlib.h:
+
+/home/sferraro/esp32/esp-idf/components/newlib/include/machine/stdlib.h:
+
+/home/sferraro/esp32/esp-idf/components/newlib/include/alloca.h:
+
+/home/sferraro/esp32/esp-idf/components/newlib/include/assert.h:
+
+/home/sferraro/esp32/esp-idf/components/esp32/include/esp_intr_alloc.h:
+
+/home/sferraro/esp32/esp-idf/components/freertos/include/freertos/FreeRTOS.h:
+
+/home/sferraro/esp32/esp-idf/components/freertos/include/freertos/FreeRTOSConfig.h:
 
 /home/sferraro/esp32/esp-idf/components/freertos/include/freertos/xtensa_config.h:
 
@@ -174,21 +155,11 @@ MatrixLed.o: /home/sferraro/esp32/MatrixLed/main/./MatrixLed.cpp \
 
 /home/sferraro/esp32/esp-idf/components/esp32/include/xtensa/xtruntime-frames.h:
 
-/home/sferraro/esp32/esp-idf/components/newlib/include/stdlib.h:
-
-/home/sferraro/esp32/esp-idf/components/newlib/include/machine/stdlib.h:
-
-/home/sferraro/esp32/esp-idf/components/newlib/include/alloca.h:
-
 /home/sferraro/esp32/esp-idf/components/esp32/include/rom/ets_sys.h:
 
 /home/sferraro/esp32/esp-idf/components/soc/esp32/include/soc/soc.h:
 
 /home/sferraro/esp32/esp-idf/components/esp32/include/esp_assert.h:
-
-/home/sferraro/esp32/esp-idf/components/newlib/platform_include/assert.h:
-
-/home/sferraro/esp32/esp-idf/components/newlib/include/assert.h:
 
 /home/sferraro/esp32/esp-idf/components/freertos/include/freertos/projdefs.h:
 
@@ -212,25 +183,17 @@ MatrixLed.o: /home/sferraro/esp32/MatrixLed/main/./MatrixLed.cpp \
 
 /home/sferraro/esp32/esp-idf/components/freertos/include/freertos/mpu_wrappers.h:
 
-/home/sferraro/esp32/esp-idf/components/freertos/include/freertos/task.h:
-
-/home/sferraro/esp32/esp-idf/components/newlib/include/limits.h:
-
-/home/sferraro/esp32/esp-idf/components/freertos/include/freertos/list.h:
-
-/home/sferraro/esp32/esp-idf/components/driver/include/driver/i2c.h:
-
-/home/sferraro/esp32/esp-idf/components/esp32/include/esp_types.h:
-
-/home/sferraro/esp32/esp-idf/components/esp32/include/esp_err.h:
-
-/home/sferraro/esp32/esp-idf/components/esp32/include/esp_intr_alloc.h:
-
 /home/sferraro/esp32/esp-idf/components/freertos/include/freertos/semphr.h:
 
 /home/sferraro/esp32/esp-idf/components/freertos/include/freertos/queue.h:
 
 /home/sferraro/esp32/esp-idf/components/freertos/include/freertos/xtensa_api.h:
+
+/home/sferraro/esp32/esp-idf/components/freertos/include/freertos/task.h:
+
+/home/sferraro/esp32/esp-idf/components/newlib/include/limits.h:
+
+/home/sferraro/esp32/esp-idf/components/freertos/include/freertos/list.h:
 
 /home/sferraro/esp32/esp-idf/components/freertos/include/freertos/queue.h:
 
@@ -254,79 +217,5 @@ MatrixLed.o: /home/sferraro/esp32/MatrixLed/main/./MatrixLed.cpp \
 
 /home/sferraro/esp32/esp-idf/components/esp32/include/esp_attr.h:
 
-/home/sferraro/esp32/esp-idf/components/esp32/include/esp_wifi.h:
-
-/home/sferraro/esp32/esp-idf/components/esp32/include/rom/queue.h:
-
-/home/sferraro/esp32/esp-idf/components/esp32/include/esp_wifi_types.h:
-
-/home/sferraro/esp32/esp-idf/components/esp32/include/esp_interface.h:
-
-/home/sferraro/esp32/esp-idf/components/esp32/include/esp_wifi_crypto_types.h:
-
-/home/sferraro/esp32/esp-idf/components/esp32/include/esp_event.h:
-
-/home/sferraro/esp32/esp-idf/components/tcpip_adapter/include/tcpip_adapter.h:
-
-/home/sferraro/esp32/esp-idf/components/esp32/include/rom/queue.h:
-
-/home/sferraro/esp32/esp-idf/components/esp32/include/esp_wifi_types.h:
-
-/home/sferraro/esp32/esp-idf/components/lwip/include/lwip/lwip/ip_addr.h:
-
-/home/sferraro/esp32/esp-idf/components/lwip/include/lwip/lwip/opt.h:
-
-/home/sferraro/esp32/esp-idf/components/lwip/include/lwip/port/lwipopts.h:
-
-/home/sferraro/esp32/esp-idf/components/newlib/include/time.h:
-
-/home/sferraro/esp32/esp-idf/components/newlib/include/machine/time.h:
-
-/home/sferraro/esp32/esp-idf/components/newlib/include/sys/time.h:
-
-/home/sferraro/esp32/esp-idf/components/newlib/include/sys/fcntl.h:
-
-/home/sferraro/esp32/esp-idf/components/newlib/include/sys/_default_fcntl.h:
-
-/home/sferraro/esp32/esp-idf/components/newlib/include/sys/stat.h:
-
-/home/sferraro/esp32/esp-idf/components/esp32/include/esp_task.h:
-
-/home/sferraro/esp32/esp-idf/components/lwip/include/lwip/lwip/debug.h:
-
-/home/sferraro/esp32/esp-idf/components/lwip/include/lwip/lwip/arch.h:
-
-/home/sferraro/esp32/esp-idf/components/lwip/include/lwip/port/arch/cc.h:
-
-/home/sferraro/esp32/esp-idf/components/newlib/include/errno.h:
-
-/home/sferraro/esp32/esp-idf/components/newlib/include/sys/errno.h:
-
-/home/sferraro/esp32/esp-idf/components/lwip/include/lwip/port/arch/sys_arch.h:
-
-/home/sferraro/esp32/esp-idf/components/lwip/include/lwip/lwip/def.h:
-
-/home/sferraro/esp32/esp-idf/components/lwip/include/lwip/lwip/ip4_addr.h:
-
-/home/sferraro/esp32/esp-idf/components/lwip/include/lwip/lwip/ip6_addr.h:
-
-/home/sferraro/esp32/esp-idf/components/lwip/include/lwip/apps/dhcpserver.h:
-
-/home/sferraro/esp32/MatrixLed/main/./caracteres.h:
-
-/home/sferraro/esp32/esp-idf/components/esp32/include/esp_system.h:
-
-/home/sferraro/esp32/esp-idf/components/esp32/include/esp_deep_sleep.h:
-
-/home/sferraro/esp32/esp-idf/components/driver/include/driver/touch_pad.h:
-
-/home/sferraro/esp32/esp-idf/components/esp32/include/esp_intr.h:
-
-/home/sferraro/esp32/esp-idf/components/esp32/include/rom/ets_sys.h:
-
-/home/sferraro/esp32/esp-idf/components/nvs_flash/include/nvs_flash.h:
-
-/home/sferraro/esp32/esp-idf/components/nvs_flash/include/nvs.h:
-
 /home/sferraro/esp32/esp-idf/components/log/include/esp_log.h:
-/home/sferraro/esp32/MatrixLed/main/./MatrixLed.cpp:
+/home/sferraro/esp32/MatrixLed/main/./SensTemp.cpp:
--- main/MatrixLed.cpp
+++ main/MatrixLed.cpp
@@ -1,11 +1,14 @@
-#include <esp_log.h>
 #include <stdio.h>
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
-#include <bitset>
+#include <driver/i2c.h>
 #include "esp_wifi.h"
+#include "caracteres.h"
 #include "esp_system.h"
 #include "sdkconfig.h"
+#include "nvs_flash.h"
+#include "esp_log.h"
+ 
 
 // See the following for generating UUIDs:
 // https://www.uuidgenerator.net/
@@ -35,148 +38,39 @@ uint32_t Matriz[]={
 0b00000000000000000000000000000000
 };
 
-const uint8_t CaracteresArray[][8]={
-{
-0b00011000,
-0b00100100,
-0b01000010,
-0b01000010,
-0b01000010,
-0b01000010,
-0b00100100,
-0b00011000
-},
-{
-0b00000010,
-0b00001010,
-0b00010010,
-0b00000010,
-0b00000010,
-0b00000010,
-0b00000010,
-0b00000010
-},
-{
-0b00111000,
-0b01000100,
-0b01000010,
-0b00000100,
-0b00001100,
-0b00011000,
-0b00110000,
-0b01111110
-},
-{
-0b00111000,
-0b01000100,
-0b00000010,
-0b00011110,
-0b00000010,
-0b00000010,
-0b01000100,
-0b00111000
-},
-{
-0b00001010,
-0b00010010,
-0b00100010,
-0b01111110,
-0b00000010,
-0b00000010,
-0b00000010,
-0b00000010
-},
-{
-0b01111110,
-0b01000000,
-0b01000000,
-0b01111000,
-0b00000100,
-0b00000010,
-0b01000100,
-0b00111000
-},
-{
-0b00011110,
-0b00100000,
-0b01000000,
-0b01000000,
-0b01011100,
-0b01100010,
-0b00100010,
-0b00011100
-},
-{
-0b01111110,
-0b00000100,
-0b00001000,
-0b00010000,
-0b00100000,
-0b01000000,
-0b01000000,
-0b01000000
-},
-{
-0b00111100,
-0b01000010,
-0b01000010,
-0b00111100,
-0b01000010,
-0b01000010,
-0b01000010,
-0b00111100
-},
-{
-0b00111100,
-0b01000010,
-0b01000010,
-0b00111110,
-0b00000010,
-0b00000010,
-0b01000100,
-0b00111000
-},
-{
-0b00111100,
-0b01000010,
-0b01000010,
-0b00111100,
-0b00000000,
-0b00000000,
-0b00000000,
-0b00000000
-},
-{
-0b00000000,
-0b01100010,
-0b01100100,
-0b00001000,
-0b00010000,
-0b00100110,
-0b01000110,
-0b00000000
-}
-};
+//static uint8_t temprature_sens_read(); 
+//static uint32_t hall_sens_read();
+void TempPres(double sali[]);
+
 void binary(uint32_t numero){
 	for (int i = 31; i >= 0; i--)
     		printf("%u",((numero >> i) & 1));
 	printf("\n");
 }
-void Pone_Car_Mat(const uint8_t Caract[], int posicion){
+void GenBarra_Mat(uint32_t Mat[],int indice){
+	uint32_t val=0;
+	for (int j=0;j<indice;j++){
+		val=val | (1<<j);
+	}
+	binary(val);
 	for (int i=0;i<Mat_Alt;i++){
-		printf("Caract : \n");
-		binary((Caract[i]));
-		printf("Caract[i] << posicion : \n");
-		binary((Caract[i] << posicion));
-		printf("Matriz[i] or Caract[i] << posicion : \n");
-		binary(Matriz[i] | (uint32_t)(Caract[i] << posicion));
-		Matriz[i]=Matriz[i] | (uint32_t)(Caract[i] << posicion);		
+		Mat[i]=val;
 	}
 }
-void Imprime_Mat(void){
+void Blanc_Mat(uint32_t Mat[]){
+	for (int i=0;i<Mat_Alt;i++){
+		Mat[i]=0b00000000000000000000000000000000;
+	}
+}
+void Pone_Car_Mat(const uint8_t Caract[], int posicion,uint32_t Mat[]){
+	for (int i=0;i<Mat_Alt;i++){
+		Mat[i]=Mat[i] | (uint32_t)(Caract[i] << posicion);		
+	}
+}
+void Imprime_Mat(uint32_t Mat[]){
 	printf("Matriz :\n");
 	for (int i=0;i<Mat_Alt;i++){
-		binary(Matriz[i]);
+		binary(Mat[i]);
 	}
 }
 void Grafica_Mat(uint32_t Matriz[],int largo){
@@ -184,43 +78,45 @@ void Grafica_Mat(uint32_t Matriz[],int largo){
 	for (int i=0;i<largo;i++){
 		val=(Matriz[0]>> i) & 1;
 		printf("Val %d.\n",val);
-		gpio_set_level(PIN1_RED1, val);
+		//gpio_set_level(PIN1_RED1, val);
 		val=(Matriz[1]>> i) & 1;
 		printf("Val %d.\n",val);
-                gpio_set_level(PIN1_RED2, val);
+                //gpio_set_level(PIN1_RED2, val);
 		val=(Matriz[2]>> i) & 1;
 		printf("Val %d.\n",val);
-                gpio_set_level(PIN2_RED1, val);
+                //gpio_set_level(PIN2_RED1, val);
 		val=(Matriz[3]>> i) & 1;
 		printf("Val %d.\n",val);
-                gpio_set_level(PIN2_RED2, val);
+                //gpio_set_level(PIN2_RED2, val);
 		val=(Matriz[4]>> i) & 1;
 		printf("Val %d.\n",val);
-                gpio_set_level(PIN3_RED1, val);
+                //gpio_set_level(PIN3_RED1, val);
 		val=(Matriz[5]>> i) & 1;
 		printf("Val %d.\n",val);
-                gpio_set_level(PIN3_RED2, val);
+                //gpio_set_level(PIN3_RED2, val);
 		val=(Matriz[6]>> i) & 1;
 		printf("Val %d.\n",val);
-                gpio_set_level(PIN4_RED1, val);
+                //gpio_set_level(PIN4_RED1, val);
 		val=(Matriz[7]>> i) & 1;
 		printf("Val %d.\n",val);
-                gpio_set_level(PIN4_RED2, val);
+                //gpio_set_level(PIN4_RED2, val);
 		vTaskDelay(50 / portTICK_PERIOD_MS);
-                gpio_set_level(PIN_CLK, 1);
+                //gpio_set_level(PIN_CLK, 1);
                 printf("Clock.\n");
                 vTaskDelay(50 / portTICK_PERIOD_MS);
-                gpio_set_level(PIN_CLK, 0);
+                //gpio_set_level(PIN_CLK, 0);
 	}
         vTaskDelay(50 / portTICK_PERIOD_MS);
-        gpio_set_level(PIN_STR, 1);
+        //gpio_set_level(PIN_STR, 1);
         vTaskDelay(50 / portTICK_PERIOD_MS);
-        gpio_set_level(PIN_STR, 0);
-        gpio_set_level(PIN_BLK, 0);
+        //gpio_set_level(PIN_STR, 0);
+        //gpio_set_level(PIN_BLK, 0);
 }
 		
 
 static void run() {
+	nvs_flash_init();
+    	system_init();
 	//gpio_config_t io_conf;
 	//io_conf.intr_type = GPIO_INTR_DISABLE;
 	//io_conf.mode = GPIO_MODE_OUTPUT;
@@ -228,29 +124,54 @@ static void run() {
 	//io_conf.pull_down_en = GPIO_PULLDOWN_DISABLE;
 	//io_conf.pull_up_en = GPIO_PULLUP_DISABLE;
 	//gpio_config(&io_conf);
-    	gpio_set_direction(PIN1_RED1, GPIO_MODE_OUTPUT);
-    	gpio_set_direction(PIN1_RED2, GPIO_MODE_OUTPUT);
-    	gpio_set_direction(PIN2_RED1, GPIO_MODE_OUTPUT);
-    	gpio_set_direction(PIN2_RED2, GPIO_MODE_OUTPUT);
-    	gpio_set_direction(PIN3_RED1, GPIO_MODE_OUTPUT);
-    	gpio_set_direction(PIN3_RED2, GPIO_MODE_OUTPUT);
-    	gpio_set_direction(PIN4_RED1, GPIO_MODE_OUTPUT);
-    	gpio_set_direction(PIN4_RED2, GPIO_MODE_OUTPUT);
-    	gpio_set_direction(PIN_BLK, GPIO_MODE_OUTPUT);
-    	gpio_set_direction(PIN_STR, GPIO_MODE_OUTPUT);
-    	gpio_set_direction(PIN_CLK, GPIO_MODE_OUTPUT);
-        gpio_set_level(PIN_BLK, 1);
-        gpio_set_level(PIN_CLK, 0);
-        printf("BLK 1.\n");
+    	
+	//gpio_set_direction(PIN1_RED1, GPIO_MODE_OUTPUT);
+    	//gpio_set_direction(PIN1_RED2, GPIO_MODE_OUTPUT);
+    	//gpio_set_direction(PIN2_RED1, GPIO_MODE_OUTPUT);
+    	//gpio_set_direction(PIN2_RED2, GPIO_MODE_OUTPUT);
+    	//gpio_set_direction(PIN3_RED1, GPIO_MODE_OUTPUT);
+    	//gpio_set_direction(PIN3_RED2, GPIO_MODE_OUTPUT);
+    	//gpio_set_direction(PIN4_RED1, GPIO_MODE_OUTPUT);
+    	//gpio_set_direction(PIN4_RED2, GPIO_MODE_OUTPUT);
+    	//gpio_set_direction(PIN_BLK, GPIO_MODE_OUTPUT);
+    	//gpio_set_direction(PIN_STR, GPIO_MODE_OUTPUT);
+    	//gpio_set_direction(PIN_CLK, GPIO_MODE_OUTPUT);
+        //gpio_set_level(PIN_BLK, 1);
+        //gpio_set_level(PIN_CLK, 0);
+       
+	printf("BLK 1.\n");
         vTaskDelay(500 / portTICK_PERIOD_MS);
         printf("BLK 0.\n");
        	vTaskDelay(200 / portTICK_PERIOD_MS);
 	//esp_deep_sleep(10000);
 	//esp_deep_sleep(10000);
 	int val=0;
-	Pone_Car_Mat(CaracteresArray[1],0);
-	Pone_Car_Mat(CaracteresArray[0],8);
-	Imprime_Mat();
+	Blanc_Mat(Matriz);
+	Imprime_Mat(Matriz);
+	Pone_Car_Mat(CaracteresArray[1],0,Matriz);
+	Pone_Car_Mat(CaracteresArray[0],8,Matriz);
+	Imprime_Mat(Matriz);
+	Grafica_Mat(Matriz,32);
+	Blanc_Mat(Matriz);
+	GenBarra_Mat(Matriz,20);
+	Imprime_Mat(Matriz);
+	double sali[2]={0};
+        TempPres(sali);
+	//float temp=( temprature_sens_read() - 32) / 1.8 ;
+        printf("Temp %f\n",sali[0]);
+	int itemp = static_cast<int>(sali[0]);
+        printf("iTemp %d\n",itemp);
+	int k=0;
+	Blanc_Mat(Matriz);
+	while (itemp > 0)
+	{	
+    		int digit = itemp%10;
+    		itemp /= 10;
+		Pone_Car_Mat(CaracteresArray[digit],k,Matriz);
+		k=k+8;
+    		printf("Digit : %d  Posicion : %d", digit,k);
+	}
+	Imprime_Mat(Matriz);
 	//while (1){
 	//	val= !val;
 	 //       printf("RED 1.\n");
@@ -285,6 +206,17 @@ static void run() {
 
 void MatrixLed(void)
 {
+	i2c_config_t conf;
+    	conf.mode = I2C_MODE_MASTER;
+    	conf.sda_io_num = GPIO_NUM_18;
+    	conf.scl_io_num = GPIO_NUM_19;
+    	conf.sda_pullup_en = GPIO_PULLUP_ENABLE;
+    	conf.scl_pullup_en = GPIO_PULLUP_ENABLE;
+    	conf.master.clk_speed = 100000;
+    	i2c_param_config(I2C_NUM_0, &conf);
+    	i2c_driver_install(I2C_NUM_0, I2C_MODE_MASTER, 0, 0, 0);
+
+
 	run();
 } // app_main
 
